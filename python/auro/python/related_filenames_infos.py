60aa6fc25b5f6bb81174e541fe3711891f2d61b5
Emile Vrijdags <emile@auro-technologies.com>
2019-03-14 16:55:42 +0100

    update python related_filenames_infos to match current status, add 2 for snippets

--- python/auro/python/related_filenames_infos.py
+++ python/auro/python/related_filenames_infos.py
@@ -4,28 +4,28 @@ Bt = Enum('BasenameTypeEnum', 'source test')
 
 basename_matchers = {
         Bt.source: ['.py'],
-        Bt.test: ['test_{name}.py']
+        Bt.test: ['test_{name}.py', '_test.py']
         }
 
 Dt = Enum('DirnameTypeEnum', 'source test')
 
 dirname_matchers = {
         Dt.source: '{base_dir}/python/{namespace}',
-        Dt.test: '{base_dir}/python/test/{namespace}'
+        Dt.test: '{base_dir}/test/python/{namespace}'
         }
 
 related_source_info = {
-        'basename_mapping':             [{'from': [Bt.test], 'to': [Bt.source]}],
-        'dirname_mapping':             [{'from': [Dt.test], 'to': [Dt.source]}],
+        'basename_mapping':  [{'from': [Bt.test], 'to': [Bt.source]}],
+        'dirname_mapping':   [{'from': [Dt.test], 'to': [Dt.source]}],
         'basename_matchers': basename_matchers,
-        'dirname_matchers':      dirname_matchers
+        'dirname_matchers':  dirname_matchers
         }
 
 related_test_info = {
-        'basename_mapping':             [{'from': [Bt.source], 'to': [Bt.test]}],
-        'dirname_mapping':             [{'from': [Dt.source], 'to': [Dt.test]}],
+        'basename_mapping':  [{'from': [Bt.source], 'to': [Bt.test]}],
+        'dirname_mapping':   [{'from': [Dt.source], 'to': [Dt.test]}],
         'basename_matchers': basename_matchers,
-        'dirname_matchers':      dirname_matchers
+        'dirname_matchers':  dirname_matchers
         }
 
 infos = {}
