8e840965b47b3bb218eb1937af8f5a6ec993d08f
Emile Vrijdags <emile@auro-technologies.com>
2021-10-06 10:38:39 +0200

    Improve messaging when changing pwd: now only prints 1 line.

--- python/auro/vim/chdir_to.py
+++ python/auro/vim/chdir_to.py
@@ -4,26 +4,23 @@ from auro.path import AuroPath
 
 def chdir_to_current_file_dir():
     path = PurePath(vim.current.buffer.name).parent
-    print("Changing dir to current file dir")
-    vim.command('cd ' + str(path))
-    print("pwd: " + str(path))
+    vim.command('silent exec "cd ' + str(path) + '"')
+    print("Changed pwd to current file dir: " + str(path))
 
 def chdir_to_module_dir():
     path = AuroPath(vim.current.buffer.name)
-    print("Changing dir to module dir")
     if path.module_dir:
-        vim.command('cd ' + path.module_dir)
-        print("pwd: " + path.module_dir)
+        vim.command('silent exec "cd ' + path.module_dir + '"')
+        print("Changed pwd to module dir: " + path.module_dir)
     else:
-        print("No module_dir")
+        print("Didn't change pwd: no git module dir found.")
 
 def chdir_to_supermodule_dir():
     path = AuroPath(vim.current.buffer.name)
-    print("Changing dir to supermodule dir")
     if path.supermodule_dir:
-        vim.command('cd ' + path.supermodule_dir)
-        print("pwd: " + path.supermodule_dir)
+        vim.command('silent exec "cd ' + path.supermodule_dir + '"')
+        print("Changed pwd to supermodule dir: " + path.supermodule_dir)
     else:
-        print("No supermodule_dir")
+        print("Didn't change pwd: no supermodule_dir")
         chdir_to_module_dir()
 
